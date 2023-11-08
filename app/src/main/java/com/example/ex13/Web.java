da3f12e6d3028b420ba705fc2578aae47c976dec
LEESEOUNGMINv <always0616@gmail.com>
2023-11-08 14:46:44 +0900

    Android

--- app/src/main/java/com/example/ex13/Web.java
+++ app/src/main/java/com/example/ex13/Web.java
@@ -17,7 +17,7 @@ public class Web {
 
 
     // cmd 창 > ipconfig 확인 .. 연결이 끊김 으로 되어있으면 연결불가
-    public static String ip = "192.168.0.29"; //본인 IP
+    public static String ip = "192.168.0.89"; //본인 IP
     // http://본인IP:8081/컨텍스트명(스프링 3번째 패키지)/   ==> 현재 포트번호가 80으로 설정했으므로 포트번호 생략
     public static String servletURL = "http://" + ip + "/gsonEx/android/"; //연결할 JSP URL
 
